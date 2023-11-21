90a8fead6376bce979aa5f7f7ad959b520fc022a
rick9711 <147951403+morty1799@users.noreply.github.com>
2023-11-21 09:10:43 -0700

    First commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+/* comment
+Multiline */
+
+// alert (*Hello again, World!');
+
+const FIRSTNAME = "Carmen";
+let lastName = "Lau";
+var lastName = "Lau"; 
+FIRSTNAME= "Carmen";
+let age = 21 + 21;
+console. log(tyeof age,age);
+console. log(FRISTNAME, lastName)
